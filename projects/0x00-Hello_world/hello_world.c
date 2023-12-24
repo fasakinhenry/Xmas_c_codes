cf7f3199811b018a83c7e60bdc08a20591e68676
Henry Fasakin <fasakinhenry@gmail.com>
2023-12-24 20:02:36 +0100

    feat: Add hello_world.c - a code that prints the phrase hello world to the terminal

---
+++ projects/0x00-Hello_world/hello_world.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of our program
+ * prints the phrase hello world
+ * return: Always 0 (success)
+ */
+
+int main(void)
+{
+	printf("Hello world");
+}
