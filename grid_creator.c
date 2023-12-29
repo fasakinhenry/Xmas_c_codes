86ee45070842cab0c44dd53392e24a2f2eac9490
Fasakin Henry <fasakinhenry@gmail.com>
2023-12-29 03:17:44 +0100

    feat: edit the grid_creator.c file

--- grid_creator.c
+++ grid_creator.c
@@ -1,25 +1,43 @@
 #include <stdio.h>
 
+int getsize(int n);
+void print_square(int n);
 int main(void)
 {
 	int n;
+	/* Get size of grid from user */
+	n = getsize(n);
+	/* Print the size of square given by the user*/
+	print_square(n);
+}
+
+/**
+ * getsize - Get the size of the square from the user
+ * @n: The size of square grid given by the user
+*/
+int getsize(int n)
+{	
 	do
 	{
 		printf("Enter the size of square grid you want: ");
 		scanf("%d", &n);
-		/* code */
 	} while (n < 1);
 
+	return (n);
+}
+
+/**
+ * Print_square - Prints square of dimension n * n
+ * @n: The size of the square grid
+*/
+void print_square(int n)
+{
 	for (int i = 0; i < n; i++)
 	{
-		/* code */
 		for (int i = 0; i < n; i++)
 		{
-			/* code */
 			printf("#");
 		}
 		printf("\n");
 	}
-	
-	
 }
