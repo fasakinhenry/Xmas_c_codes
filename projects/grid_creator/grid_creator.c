8e3a3d4970ddd311a93a4566a5204dbe4a8f4aec
Fasakin Henry <fasakinhenry@gmail.com>
2023-12-31 06:31:28 +0100

    refactor: Improve code structure in grid_creator.c
    Refactor code to adhere to betty standards

--- projects/grid_creator/grid_creator.c
+++ projects/grid_creator/grid_creator.c
@@ -2,23 +2,32 @@
 
 int getsize(int n);
 void print_square(int n);
+/**
+ * main - The entry point of our program
+ * This program prints a square grid with # symbol
+ * The number of grid is determined by the user
+ * Return: Always 0 (success)
+ * On error, -1 is returned
+*/
 int main(void)
 {
-	int n;
+	int n = 0;
 	/* Get size of grid from user */
 	n = getsize(n);
 	/* Print the size of square given by the user*/
 	print_square(n);
+
+	return (0);
 }
 
 /**
  * getsize - Get the size of the square from the user
  * @n: The size of square grid given by the user
+ * Return: The size of the square grid givven by user
 */
 int getsize(int n)
-{	
-	do
-	{
+{
+	do {
 		printf("Enter the size of square grid you want: ");
 		scanf("%d", &n);
 	} while (n < 1);
@@ -27,14 +36,16 @@ int getsize(int n)
 }
 
 /**
- * Print_square - Prints square of dimension n * n
+ * print_square - Prints square of dimension n * n
  * @n: The size of the square grid
 */
 void print_square(int n)
 {
-	for (int i = 0; i < n; i++)
+	int i = 0;
+
+	for (; i < n; i++)
 	{
-		for (int i = 0; i < n; i++)
+		for (; i < n; i++)
 		{
 			printf("#");
 		}
