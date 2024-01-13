1cdf600e424350e106ff65accba39767004f77e7
Fasakin Henry <fasakinhenry@gmail.com>
2024-01-13 16:47:52 +0100

    feat: print the +ve or _ve status of a randim number

---
+++ concepts/0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - prints a statement to shoe if a random value is +ve or -ve
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else
+		printf("%d is negative\n", n);
+	
+	return (0);
+}
