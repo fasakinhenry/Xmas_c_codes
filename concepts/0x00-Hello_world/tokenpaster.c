d769def06513ec362e8f74d0cc4971bdd80a3631
Fasakin Henry <fasakinhenry@gmail.com>
2024-01-13 14:35:14 +0100

    Feat: learn and practice tokenpaster.c

---
+++ concepts/0x00-Hello_world/tokenpaster.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#define tokenpaster(n) printf("token" #n " = %d\n", token##n)
+
+/**
+ * main - demonstrates the tokenpaster functionality
+ * return: Always 0 (success)
+ * one error, -1 is returned
+*/
+
+int main(void)
+{
+	int token34 = 40;
+	tokenpaster(34);
+	return (0);
+}
