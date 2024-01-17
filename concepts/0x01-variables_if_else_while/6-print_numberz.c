87214fd6cd71f25374a16293212a77407924f4e8
Fasakin Henry <fasakinhenry@gmail.com>
2024-01-17 05:02:01 +0100

    style: Remove the curly braces in file 6-print_numberz and use indent

--- concepts/0x01-variables_if_else_while/6-print_numberz.c
+++ concepts/0x01-variables_if_else_while/6-print_numberz.c
@@ -17,9 +17,7 @@ int main(void)
 	int i;
 
 	for (i = 0; i < count; i++)
-	{
 		putchar(i + '0');
-	}
 	putchar('\n');
 	return (0);
 }
---
+++ concepts/0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of our program
+ * Print all the letters of the alphabet in reverse
+ * Putchar can only be used and cannot be used more than twice
+ * Return: always 0 (Success)
+ * on error, -1 is returned
+*/
+
+int main(void)
+{
+	int letter;
+	
+	return (0);
+}
