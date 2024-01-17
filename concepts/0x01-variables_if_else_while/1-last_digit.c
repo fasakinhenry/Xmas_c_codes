92fc7480b1e248f339f7edc9e7325aec4c4f71c6
Fasakin Henry <fasakinhenry@gmail.com>
2024-01-17 05:06:51 +0100

    Feat: Add code for the 1-last_digit concpt
    This code prints is the last digit is greater than 0 and less than 6, is equal to 0 or even greater than 5

--- concepts/0x01-variables_if_else_while/1-last_digit.c
+++ concepts/0x01-variables_if_else_while/1-last_digit.c
@@ -1,18 +1,29 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+#include <stdio.h>
 
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point of the program
+ * the program gets the last digit of a random number,
+ * then prints if it is greater than 5,
+ * or less than 6 and not 0
+ * or is equal to o
+ * Return: Always 0(succcess)
+ * on error, -1 is returned
+*/
+
 int main(void)
 {
 	int n, last_digit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	last_digit= n % 10;
-	if (last_digit < 0)
-		last_digit = -last_digit;
-	
-	
+	last_digit = n % 10;
+	if (last_digit > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
+	else if (last_digit == 0)
+		printf("Last digit of %d is %d and is 0\n", n, last_digit);
+	else
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
 	return (0);
 }
